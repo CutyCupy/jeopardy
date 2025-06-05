a1ff0d2bcc9bfb7000bc3314afd3e5e5b0e3e931
CutyCupy <alex.ciupka@gmx.de>
2025-06-05 17:32:30 +0200

    first commit

---
+++ src/test/java/de/ciupka/jeopardy/JeopardyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ciupka.jeopardy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeopardyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
