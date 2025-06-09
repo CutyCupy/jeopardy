f50ad2a9082adb9f95427c557b384e4151b413ee
CutyCupy <alex.ciupka@gmx.de>
2025-06-09 22:52:27 +0200

    First JavaDoc comments for join and addPlayer Functions

--- src/main/java/de/ciupka/jeopardy/controller/MessageController.java
+++ src/main/java/de/ciupka/jeopardy/controller/MessageController.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import de.ciupka.jeopardy.configs.UserPrincipal;
@@ -21,11 +22,32 @@ public class MessageController {
     @Autowired
     private NotificationService notifications;
 
+    /**
+     * Websocket Message Handler for join requests that will add the requesting
+     * {@code Player} to the lobby and send a lobby update to all other connected
+     * players. For furthe information on the adding {@code Player} to the
+     * {@code LobbyService} see {@link de.ciupka.jeopardy.game.LobbyService#addPlayer LobbyService.addPlayer}
+     * 
+     * @param name      The name of the {@code Player} that wants to join.
+     * @param principal The {@code de.ciupka.jeopardy.configs.UserPrincipal} that
+     *                  was created when a WebSocket connection was established.
+     * @return True if a player was added to the lobby. If a player already exists
+     *         with {@code name}, it will return false.
+     * @throws Exception
+     */
     @MessageMapping("/join")
-    public void join(String message, Principal principal) throws Exception {
-        this.lobby.addPlayer(((UserPrincipal) principal).getID(), message);
+    @SendToUser("/topic/join")
+    public boolean join(String name, Principal principal) throws Exception {
+        final boolean added = this.lobby.addPlayer(((UserPrincipal) principal).getID(), name);
 
+        /**
+         * TODO: Might be necessary to send more data on lobby updates.
+         * In general it might be smart to also send the 'reason' for a lobby update
+         * with extra data based on reasoning.
+         */
         notifications.broadcastMessage(LOBBY_UPDATE, this.lobby.getPlayers());
+
+        return added;
     }
 
 }
--- src/main/java/de/ciupka/jeopardy/game/LobbyService.java
+++ src/main/java/de/ciupka/jeopardy/game/LobbyService.java
@@ -17,14 +17,30 @@ public class LobbyService {
         this.players = new ArrayList<>();
     }
 
-    public void addPlayer(UUID uuid, String name) {
+    /**
+     * addPlayer either adds a {@code Player} to the lobby or updates the
+     * {@code uuid} of the existing {@code Player} when someone with {@code name}
+     * already exists.
+     * 
+     * @param uuid The {@code UUID} of the Websocket Client that tries to be added
+     *             to the lobby.
+     * @param name The name that the new player wants to have.
+     * @return True if a player was added to the lobby. If a player already exists
+     *         with {@code name}, it will return false.
+     */
+    public boolean addPlayer(UUID uuid, String name) {
         Optional<Player> player = this.players.stream().filter((p) -> p.getName().equals(name))
                 .findFirst();
         if (player.isPresent()) {
+            // TODO: Think about a better solution for returning players.
+            // This Method relies heavily on trust, that noone hijacks another player by
+            // simply using their name.
             player.get().setUuid(uuid);
-            return;
+            return false;
         }
         this.players.add(new Player(uuid, name));
+
+        return true;
     }
 
     public Player[] getPlayers() {
